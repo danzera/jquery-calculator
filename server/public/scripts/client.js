b2d5ea31ea3b8914a06cf96708434db7d06f8baa
Dan Zera <dczera@gmail.com>
2017-03-18 00:32:22 -0500

    basic DOM elements setup

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,13 @@
+var operator;
+
 $(document).ready(function() {
-  console.log('jquery totes sourced');
+  // handle calculator button click events
+  $('.calculator').on('click', 'button', function(e) {
+    // pull name from the button that was clicked
+    var id = this.name;
+    operator = id;
+    console.log(id);
+    //$('button').removeClass('highlight');
+    $(this).addClass('highlight');
+  });
 });
