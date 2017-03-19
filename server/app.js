b19c05b41c36c1cdab5671f8e38cef20c90a6970
Dan Zera <dczera@gmail.com>
2017-03-18 20:13:24 -0500

    add basic button event handlers and AJAX POST call

--- server/app.js
+++ server/app.js
@@ -5,19 +5,30 @@ var app = express();
 var path = require('path');
 // import body-parser
 var bodyParser = require('body-parser');
+// further requirement to use bodyParser
+app.use(bodyParser.urlencoded({extended: true}));
 // set port
 app.set('port', 4567);
 
 // set default path
 app.use(express.static('server/public'));
-// serve up index file for base URL
+
+// base URL 'GET'
 app.get('/', function(req, res) {
   console.log('base URL hit');
+  // serve up index.html file
   res.sendFile(path.resolve('server/public/views/index.html'));
 });
 
+// '/calculate' 'POST' request
+app.post('/calculate', function(req, res) {
+  console.log('/calculate path hit');
+  var calculation = req.body;
+  console.log(calculation);
+  res.send('heard you on path /calculate');
+});
 
 // listen on port
 app.listen(app.get('port'), function() {
-  console.log('totes listening on port ', app.get('port'));
+  console.log("We're live on port ", app.get('port'));
 });
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,13 +1,45 @@
+// global variable assigned by click-handler and
+// sent over in AJAX call by the submit button
 var operator;
 
 $(document).ready(function() {
-  // handle calculator button click events
-  $('.calculator').on('click', 'button', function(e) {
-    // pull name from the button that was clicked
-    var id = this.name;
-    operator = id;
-    console.log(id);
-    //$('button').removeClass('highlight');
+  // handle operator button click events
+  $('.operators').on('click', 'button', function() {
+    // pull name from the button that was clicked and
+    // assign it to the operator variable
+    operator = this.name;
+    // remove highlight class from any previous operator button that was clicked
+    $('button').removeClass('highlight');
+    // add highlight class to current operator button that is selected
     $(this).addClass('highlight');
+    console.log("current operator selected: ", operator);
+  });
+
+  $('.calculator').on('submit', function(e) {
+    // prevent auto-refresh of the page
+    e.preventDefault();
+    // store numbers entered by the user
+    var x = parseInt($('#numOneInCalculator').val());
+    var y = parseInt($('#numTwoInCalculator').val());
+    console.log("x = ", x);
+    console.log("y = ", y);
+    // store numbers and the currently selected operator in an object
+    var calculation = {
+      numOne: x,
+      numTwo: y,
+      operation: operator
+    };
+    console.log("object being sent to the server: ", calculation);
+    // AJAX 'POST' call to the server
+    // sending object to be calculated
+    // receiving calculation result
+    $.ajax({
+      type: 'POST',
+      url: '/calculate',
+      data: calculation,
+      success: function(res) {
+        console.log(res);
+      }
+    });
   });
 });
